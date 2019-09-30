636c7f424762dde777fa707c13d1a94ec0c10eda
Chris Watson <watsonc135@gmail.com>
2019-09-30 05:59:49 +0000

    Added more detail and a database for future use.

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -1,4 +1,15 @@
 class QuizzesController < ApplicationController
   def index
+    @quiz = Quiz.order("RANDOM()").first
+  end
+
+  def create
+    @quiz = Quiz.create(quiz_params)
+  end
+
+  private
+
+  def quiz_params
+    params.require(:quiz).permit(:answer)
   end
 end
